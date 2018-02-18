b81681e8f6ba35a962a9b5649b6c057fdae983e9
robbywashere <robertapolana@gmail.com>
2018-02-18 21:47:17 +0700

    grid of services

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -16,7 +16,14 @@ const TinyIcon = ({ src, style }) => (
   </span>
 )
 
+const serviceCardData = [{ text:"Mowing" , subtext:"Weekly, Bi-Weekly & Monthly scheduling available"  },
+  { text:"Landscaping" , subtext:"Let us add the nice touch" },
+  { text:"Spring & Fall cleanup" , subtext:"Leaves and such" },
+  { text:"Tree trimming and Pruning" , subtext:"Branches mannnn...."  },
+  { text:"Call now!"  }];
+
 const ServiceCard  = ({ style, text, subtext }) =>(
+
   <div style={style} className="service-card">
     <h4>{ text }</h4>
     <br/>
@@ -56,7 +63,7 @@ module.exports = ()=>(
     </Container>
     <Container fluid={true}>
       <Row>
-        <Col className="section1" xs={12} sm={12} md={6}>
+        <Col className="section1">
           <section>
             <h3 className="section-head">
               <TinyIcon src={grass}  />
@@ -74,23 +81,23 @@ module.exports = ()=>(
             </p>
           </section>
         </Col>
-        <Col className="column2 section2" xs={12} sm={12} md={6}>
+      </Row>
+      <Row>
+        <Col className="column2 section2">
           <section>
             <h3 className="section-head">
               <TinyIcon src={thumb}  />
               <span className="section-title" >Our services include ...</span>
             </h3>
-            
-            <div className="service-cards">
-              <ServiceCard text="Mowing" subtext="Weekly, Bi-Weekly & Monthly scheduling available" />
-              <ServiceCard text="Landscaping" subtext="Let us add the nice touch"/>
-              <ServiceCard text="Spring & Fall cleanup" subtext="Leaves and such"/>
-              <ServiceCard text="Tree trimming and Pruning" subtext="Branches mannnn...." />
-              <ServiceCard text="Call now!" />
-            </div>
-          </section>
-        </Col>
-      </Row>
-    </Container>
-  </main>
+
+            <div className="service-cards" style={{ 
+            }}>
+            { serviceCardData.map(ServiceCard) } 
+            <ServiceCard style={{ visibility: "hidden" }} />
+          </div>
+        </section>
+      </Col>
+    </Row>
+  </Container>
+</main>
 )
