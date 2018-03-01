22a62b05a5551d407ab76144f4784c8123dabaa6
robbywashere <robertapolana@gmail.com>
2018-03-01 15:16:18 +0700

    ok cool

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,24 +15,59 @@ import thumb from '../img/thumbsup.svg';
 
 import { green } from '../styles/colors';
 import { VertCentRow, Icon } from '../shared';
+import leaf from '../img/leaf.svg';
+import leafs from '../img/leafs.svg';
+import star from '../img/star.svg';
+
+import visa from '../../node_modules/payment-icons/svg/mono/visa.svg';
+import mastercard from '../../node_modules/payment-icons/svg/mono/mastercard-old.svg';
+import amex from '../../node_modules/payment-icons/svg/mono/amex.svg';
+import paypal from '../../node_modules/payment-icons/svg/mono/paypal.svg';
 
 import woman from '../img/walmartwoman.jpg';
 import ted from '../img/ted.jpg';
 import trapped from '../img/trappedman.jpg';
+import quoteopen from '../img/quote-open.svg';
 
 
 //const SlidingText = (...words) => (<div className="slidingVertical">{words.map(w=><span>{w}</span>)}</div>)
 
 const Heart = () => (<img src={heart} alt="love" className="love" style={{ width: '100px', height: '100px' }} alt="<3"/>)
 
+const PaymentTypes = ()=> (<div className="payment-types">
+  { [visa, mastercard, amex, paypal].map( (src,i) => <img key={i} src={src} /> ) }
+</div>
+)
 const Steven = () => (<img src={steven} style={{ maxWidth: 'initial', width: '250px', height: '250px', borderRadius: '50%' }} />);
 const Map = () => (<img src={map} style={{ maxWidth: 'initial', width: '250px', height: '250px', borderRadius: '50%' }} />);
 
+
+const LeafsHr = (<LeafHr img={ leafs } imgStyle={ { width: '40px'} }/>)
+const LeafHr = ({ style, imgStyle, img = leaf }) => (
+  <div className="leaf-hr" style={ {...style } }>
+    <img src={img}  style={ { ...imgStyle } } />
+  </div>
+)
+
+const Quote = ({ text, author, img })=> (
+  <Col  lg={4} md={6} xs={12}>
+    <div className='quote-container'>
+      <blockquote>
+        <p>{ text }</p>
+        <footer>
+          <div className="author-img" style={{ backgroundImage: `url(${img})`}} />
+          <div>&#8212;{ author }</div>
+          <Fivestars />
+        </footer>
+      </blockquote>
+    </div>
+  </Col>)
 
 const PCent = ({ children, style }) => (<p style={ { ...style, textAlign: "center" } }>{ children }</p>)
 
 const Cent = ({ children, style }) => (<div style={{ ...style, textAlign: "center" }}>{ children }</div>)
 
+const Fivestars = ()=> (<div className="stars"> { [1,2,3,4,5].map(i=>(<img src={star} />)) }</div>)
 
 const Page = () => (
   <main role="main" id="home">
@@ -46,11 +81,11 @@ const Page = () => (
         </VertCentRow>
       </Container>
       <div className="display-3 sub-pitch">
-        <p>At Lawn and Order Landscaping, it is our mission to create satisfied customers by delivering superior service, quality products along with integrity and 100% customer satisfaction.</p>
+        <h2>At Lawn and Order Landscaping, it is our mission to create satisfied customers by delivering superior service, quality products along with integrity and 100% customer satisfaction.</h2>
         <div style={{ textAlign: "center" }}>
           <a className="btn btn-primary btn-lg" 
             href="#" 
-            style={{color: '#FFF'}}
+            style={{color: '#FFF', display: 'none' }}
             role="button">Learn More &raquo;</a>
         </div> 
       </div>
@@ -58,6 +93,10 @@ const Page = () => (
     </div>
 
     <Container fluid>
+
+      <header>
+        <h3 className="centered">Meet Our 5 Star Services</h3>
+      </header>
       <Row style={{ } }className="service-icons" >
         <Col md={3} >
           <div>
@@ -84,56 +123,52 @@ const Page = () => (
           </div>
         </Col>
       </Row>
-      <Row className="aboutus">
-        <section>
-          <header>
-            <h3>Quick and easy online billing</h3>
-          </header>
-          <p> We accept Visa, mastercard and discover</p>
-        </section>
+      <LeafHrs$ img={ leafs } imgStyle={ { width: '40px'} }/>
+      <Row className="payment-section">
+        <Col xs={12}>
+          <section>
+            <header>
+              <h3 className="centered">Quick and Easy online billing</h3>
+            </header>
+            <PaymentTypes />
+            <em>We accept Cash, Visa, Mastercard, Discover and Paypal</em>
+            <em>We offer automatic no-fuss weekly, bi-weekly and monthly payment plans</em>
+            <em>Or have us send your invoice right to your inbox!</em>
+          </section>
+        </Col>
       </Row>
-
+      <LeafHr style={ { marginBottom: '-31px' } } imgStyle={ { background: 'initial' }} />
       <Row className="testimony">
         <Col xs={12}>
           <section>
             <header>
               <h3>Here's what people are saying about Lawn and Order Landscaping...</h3>
             </header>
-            <div className="quotes">
-              <blockquote>
-                <p> 
-                  Steven showed up half naked and bleeding from his eyes, my kinda guy! 
-                </p>
-                <span><div className="img" style={{ backgroundImage: `url(${ted})` }} />- Ted from the feed store</span>
-              </blockquote>
-              <blockquote>
-                <p> 
-                  One of the lawn dudes had a weird odor about him, like burning tires and failure, my two favorite scents 
-                </p>
-                <span><div className="img" style={{ backgroundImage: `url(${woman})`}} />- Some woman at Walmart</span>
-              </blockquote>
-              <blockquote>
-                <p> 
-                  I have no idea where I am right now please, could someone call for help?
-                </p>
-                <span><div className="img" style={{ backgroundImage: `url(${trapped})` }} />- Some guy stuck somewhere weird</span>
-              </blockquote>
-            </div>
+            <Row className="five-star-quotes">
+              <Quote author="Ted from the feed store" img={ ted } text="Steven showed up half naked and bleeding from his eyes, my kinda guy!" />
+              <Quote author="Some woman at Walmart" img={ woman } text="One of the lawn dudes had a weird odor about him, like burning tires and failure, my two favorite scents" />
+              <Quote author="Trapped Man" img={ trapped } text="I have no idea where I am right now please, could someone call for help?" />
+            </Row>
           </section>
         </Col>
       </Row>
-      <hr/>
+      <LeafHr />
       <Row className="aboutus">
         <Col xs={12}>
           <section>
             <header>
-              <h3>Meet your neighborhood lawn care specialist</h3>
+              <h3 className="centered">Meet your neighborhood lawn care specialist</h3>
             </header>
             <div className="mrceo">
               <div className="img" style={{ backgroundImage: `url(${steven})` }} />
               <h4> Steven Slaminka, Lawn and Order CEO </h4>
-              <p>
-              </p>
+              <Row>
+                <Col md={ { size: 6, offset: 3 } }  xs={12} >
+                  <p>
+                    "It is our mission at Lawn and Order Landscaping to create satisfied customers by delivering superior service, quality products along with integrity and 100% customer satisfaction."
+                  </p>
+                </Col>
+              </Row>
             </div>
           </section>
         </Col>
